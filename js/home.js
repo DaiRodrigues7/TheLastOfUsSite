ebf0f1e080754b1982d1cbfc3b816b35c5959f1e
dai <dailanne2005@icloud.com>
2025-07-14 16:59:31 -0300

    Atualização acrescentando seção para fanarts, cards com foto e breve descrição + créditos do artista.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
-const carrossel = document.getElementById('carrossel-imagens');
-const imgs = carrossel.children;
+const carrossel = document.getElementById('carrossel-imagens'); //guardando variável
+const imgs = carrossel.children; // pega todos os filhos diretos de carrossel (as imagens) e guarda em imgs
 const imgLargura = 516; // 500px + 16px gap
 let indice = 0;
 
@@ -7,7 +7,14 @@ function moverCarrossel(dir) {
   indice = (indice + dir + imgs.length) % imgs.length;
   carrossel.scrollTo({ left: indice * imgLargura, behavior: 'smooth' });
 }
+/*
+Dir é a direção desejada;
+Atualiza o índice da imagem;
+% imgs.length garante que o índice volte ao começo ou ao final (fica circular / loop);
+scrollTo({ ... }) faz o container carrossel rolar horizontalmente;
+*/
 
+//abrir imagem do carrossel
 document.querySelectorAll('.carrossel-imagens img').forEach(img => {
   img.addEventListener('click', () => {
     document.getElementById('modal-img').src = img.src;
@@ -15,6 +22,15 @@ document.querySelectorAll('.carrossel-imagens img').forEach(img => {
   });
 });
 
-function fecharModal() {
+//abrir imagem na parte de fanart
+document.querySelectorAll('.galeria .card img').forEach(img => {
+  img.addEventListener('click', () => {
+    document.getElementById('modal-img').src = img.src;
+    document.getElementById('modal').style.display = 'flex';
+  });
+});
+
+function fecharModal() { 
   document.getElementById('modal').style.display = 'none';
 }
+//esconde o modal quando chamado
